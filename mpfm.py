9ad4e0f8461f4f5edd1fab8967ac99618050a3e8
michael <michael.catchen@colorado.edu>
2019-01-31 17:49:15 -0700

    2deme updates

--- mpfm.py
+++ mpfm.py
@@ -15,57 +15,42 @@ def main():
     i = 0
     if (args['BATCH']):
         mig_space = [0.01, 0.03, 0.05]
-        locus_space = [0.2, 0.35, 0.5]
-        k_space = [300, 600, 900]
-        n_rep = 10
+        locus_space = [0.01, 0.1, 0.3]
+        k_space = [1500, 2500, 3500, 4500]
+        n_ef_space = [1,2,3]
+        ef_diff_space = [0.1, 0.5, 0.9]
+        n_rep = 30
 
-        print('Num runs: %d' % (len(mig_space)*len(locus_space)*len(k_space)*n_rep))
+        print('Num runs: %d' % (len(n_ef_space)*len(ef_diff_space)*len(mig_space)*len(locus_space)*len(k_space)*n_rep))
 
+        treatment_ct = 0
         for mig_rate in mig_space:
             for locus_weight in locus_space:
                 for k_mean in k_space:
-                    path = 'K%d_LW%.2f_MR%.2f_rep' % (k_mean, locus_weight, mig_rate)
+                    for n_ef in n_ef_space:
+                        for ef_diff in ef_diff_space:
+                            path = 'K%d_LW%.2f_MR%.2f_%dEF_%.2fDIFF_rep' % (k_mean, locus_weight, mig_rate, n_ef, ef_diff)
+                            treatment_ct += 1
+                            for rep in range(n_rep):
+                                params = {}
+                                for param in param_table:
+                                    params[param] = param_table[param]['default']
 
-                    for rep in range(n_rep):
-                        params = {}
-                        for param in param_table:
-                            params[param] = param_table[param]['default']
 
+                                params["MIGRATION_RATE"] = mig_rate
+                                params["MEAN_LOCUS_WEIGHT"] = locus_weight
+                                params["PATCH_K_MEAN"] = k_mean
 
-                        params["MIGRATION_RATE"] = mig_rate
-                        params["MEAN_LOCUS_WEIGHT"] = locus_weight
-                        params["PATCH_K_MEAN"] = k_mean
+                                params["NUM_GENERATIONS"] = 500
+                                params["NUM_OF_CHROMOSOMES"] = chr
+                                params["DATA_DIRECTORY"] = path + str(rep)
+                                params["RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                params["EF_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                params["PATCH_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                params["GENOME_RANDOM_SEED"] = np.random.randint(0, 100000000)
 
-                        params["DATA_DIRECTORY"] = path + str(rep)
-                        params["RANDOM_SEED"] = np.random.randint(0, 100000000)
-                        params["EF_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                        params["PATCH_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                        params["GENOME_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                                create_batch_run_file(this_dir, params, treatment_ct)
 
-                        print_run(params, i, rep)
-                        run = start_run(params)
-                        i += 1
-        '''
-        #param_list = read_batch_file(args['BATCH'][0])
-
-        #for i,params in enumerate(param_list):
-            #num_rep = int(params['NUM_REPLICATES'])
-
-            for rep in range(num_rep):
-                this_rep_params = copy.deepcopy(params)
-                this_rep_params["DATA_DIRECTORY"] = params["DATA_DIRECTORY"] + str(rep)
-                this_rep_params["RANDOM_SEED"] = np.random.randint(0, 100000000)
-                this_rep_params["EF_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                this_rep_params["PATCH_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                this_rep_params["GENOME_RANDOM_SEED"] = np.random.randint(0, 100000000)
-
-                os.chdir(this_dir)
-                create_batch_run_file(this_dir, this_rep_params)
-
-                #print_run(this_rep_params, i, rep)
-                #p = multiprocessing.Process(target=start_run, args=(this_rep_params,))
-                #p.start()
-        '''
     else:
         params = {}
         for param in param_table:
