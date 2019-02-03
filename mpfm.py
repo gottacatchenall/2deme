dab0524c1eae7f7b7ee0642c782dab16f8387902
Michael Catchen <mica5688@login11.rc.int.colorado.edu>
2019-02-03 11:22:38 -0700

    updates

--- mpfm.py
+++ mpfm.py
@@ -14,40 +14,40 @@ def main():
     print_info()
     i = 0
     if (args['BATCH']):
-        mig_space = [0.01, 0.03, 0.05]
-        locus_space = [0.01, 0.05, 0.1]
-        k_space = [2500, 3500, 4500, 5500]
-        n_ef_space = [1,2,3]
+        mig_space = [0.001, 0.0005]
+        locus_space = [0.1, 0.2, 0.3]
+        k_space = [1000,2000,3000]
         n_rep = 30
 
-        print('Num runs: %d' % (len(n_ef_space)*len(ef_diff_space)*len(mig_space)*len(locus_space)*len(k_space)*n_rep))
+        print('Num runs: %d' % (len(mig_space)*len(locus_space)*len(k_space)*n_rep))
 
         treatment_ct = 0
         for mig_rate in mig_space:
             for locus_weight in locus_space:
                 for k_mean in k_space:
-                    for n_ef in n_ef_space:
-                            path = 'K%d_LW%.2f_MR%.2f_%dEF_rep' % (k_mean, locus_weight, mig_rate, n_ef)
-                            treatment_ct += 1
-                            for rep in range(n_rep):
-                                params = {}
-                                for param in param_table:
-                                    params[param] = param_table[param]['default']
+                        path = 'prod3_K%d_LW%.2f_MR%.3f_rep' % (k_mean, locus_weight, mig_rate)
+                        treatment_ct += 1
+                        for rep in range(n_rep):
+                            params = {}
+                            for param in param_table:
+                                params[param] = param_table[param]['default']
 
 
-                                params["MIGRATION_RATE"] = mig_rate
-                                params["MEAN_LOCUS_WEIGHT"] = locus_weight
-                                params["PATCH_K_MEAN"] = k_mean
+                            params["MIGRATION_RATE"] = mig_rate
+                            params["MEAN_LOCUS_WEIGHT"] = locus_weight
+                            params["PATCH_K_MEAN"] = k_mean
 
-                                params["NUM_GENERATIONS"] = 500
-                                params["NUM_OF_CHROMOSOMES"] = chr
-                                params["DATA_DIRECTORY"] = path + str(rep)
-                                params["RANDOM_SEED"] = np.random.randint(0, 100000000)
-                                params["EF_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                                params["PATCH_RANDOM_SEED"] = np.random.randint(0, 100000000)
-                                params["GENOME_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                            params["NUM_GENERATIONS"] = 2000
+                            params["CENSUS_FREQ"] = 50
+                            params["NUM_OF_LOCI"] = 100
+                            params["NUM_LOCI_PER_EF"] = 50
+                            params["DATA_DIRECTORY"] = path + str(rep)
+                            params["RANDOM_SEED"] = np.random.randint(0, 100000000)
+                            params["EF_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                            params["PATCH_RANDOM_SEED"] = np.random.randint(0, 100000000)
+                            params["GENOME_RANDOM_SEED"] = np.random.randint(0, 100000000)
 
-                                create_batch_run_file(this_dir, params, treatment_ct)
+                            create_batch_run_file(this_dir, params, treatment_ct)
 
     else:
         params = {}
